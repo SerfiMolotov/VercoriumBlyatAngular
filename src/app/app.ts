5d384298d6c6b296774daca2f24ce4309d19b512
bporignaux <baptiste.prx8@gmail.com>
2026-03-30 17:40:54 +0200

    fdsfsdf

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReleveListeComponent } from './features/releves/components/releve-liste/releve-liste';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReleveListeComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('vem-angular');
